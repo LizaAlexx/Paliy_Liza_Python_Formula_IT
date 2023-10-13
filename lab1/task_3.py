0ad4738d8e81fbe303da9fe4d30b2543da602a28
LizaAlexx <141029567+LizaAlexx@users.noreply.github.com>
2023-10-13 23:58:08 +0300

    Add files via upload

---
+++ lab1/task_3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+count = len(list_players)
+
+team_1 = list_players[:3]
+team_2 = list_players[3:]
+
+print(team_1)
+print(team_2)
